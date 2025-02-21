c13324f26adf5209e1862c4b0f1fad2b0903e0cc
Nithinkumar-05 <nithineruventi@outlook.com>
2025-02-21 20:08:54 +0530

    change in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { HashRouter as Router,Routes,Route } from 'react-router-dom'
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import '@coreui/coreui/dist/css/coreui.min.css'
